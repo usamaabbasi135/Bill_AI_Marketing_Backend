830080ad8645b895b00a31aa47d3a8934d72f017
Usama Abbasi <usamahafeez.abbasi1234@gmail.com>
2025-11-01 15:58:43 +0500

    chore: add .gitignore for Python cache files

--- app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.models.tenant import Tenant
-from app.models.user import User
-from app.models.company import Company
-from app.models.post import Post
-from app.models.profile import Profile
-from app.models.email import Email
-from app.models.tenant_setting import TenantSetting
